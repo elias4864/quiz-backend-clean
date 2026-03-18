bd64992d9bf4c7077b8538df514bd0d3b86e9a6f
Elias Kaiser <elias.kaiser@gmx.ch>
2026-03-18 11:34:25 +0100

    User Klasse wird hinzugefügt

--- src/main/java/com/wiss/quizbackend/config/WebConfig.java
+++ src/main/java/com/wiss/quizbackend/config/WebConfig.java
@@ -7,6 +7,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+
+    //Spring Security mit den Schichten
+
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/api/questions/**") // <- Alle /api/questions/* Endpoints
